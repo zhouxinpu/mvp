becc3583f6d176df5aba21647a1aefa7328889b4
zhouxinpu <zhouxinpu@enmonster.com>
2019-11-22 19:31:16 +0800

    login修改

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -16,33 +16,62 @@
 	        {{item.title}}
 	      </view>
 	    </view>
-	    <view class="content"></view>
+	    <view class="content">
+				<view v-if="active=='微信登录'" class="content-wechat">
+					<button type="primary">微信一键登录</button>
+				</view>
+				<view v-else-if="active=='短信登录'" class="content-phone">
+					<view class="form">
+						<input type="number" placeholder="输入手机号" />
+						<input type="number" placeholder="输入验证码" />
+					</view>
+					<button class="btn">登录</button>
+				</view>
+				<view v-else-if="active=='取号机登录'" class="content-machine">machine</view>
+			</view>
 	    <view class="bottom-tip">
 	      <view class="tip-left">
+					<checkbox class="cb" @change="cbHandleChange" :checked="checked" color="#05c160" />
 	        <text @tap="alertTip">使用条款及隐私通知</text>
 	      </view>
 	      <view class="tip-right">
-	
+					<view class="btn">热线</view>
+					<view class="btn">en</view>
 	      </view>
 	    </view>
 	  </view>
+		<uni-popup ref="popup" type="center">
+			<view class="pop">
+				<view class="pop-top">
+					top
+				</view>
+				<view class="pop-content">
+					content
+				</view>
+				<view class="pop-bottom">
+					bottom
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
 <script>
+	import uniPopup from "@/components/uni-popup/uni-popup.vue"
 	export default {
 		data() {
 			return {
-				active: '微信登陆',
+				active: '短信登录',
 				tabs: [
-					{ title: '微信登陆', pic: 'wechat'},
-					{ title: '短信登陆', pic: 'phone'},
-					{ title: '取号机登陆', pic: 'others'}
+					{ title: '微信登录', pic: 'wechat'},
+					{ title: '短信登录', pic: 'phone'},
+					{ title: '取号机登录', pic: 'others'}
 				],
 				checked: true,  //复选框
 				canIUse: wx.canIUse('button.open-type.getUserInfo')
 			}
 		},
+		components: {uniPopup},
 		onLoad() {
 
 		},
@@ -51,86 +80,134 @@
 				this.active = item.title
 			},
 			alertTip:function(){
-				
-			},
-			onChange:function(){
-				console.log(1);
-				this.setData({
-					checked: !this.data.checked
-				})
+				this.$refs.popup.open()
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	/* login */
-	.container{
-	  width:100vw;
-	  height:100vh;
-	  position: relative;
+/* login */
+.container{
+	width:100vw;
+	height:100vh;
+	position: relative;
+
+	 .bg-login{
+		position: absolute;
+		width:100%;
+		height: 100%;
 	}
-	.container .bg-login{
-	  position: absolute;
-	  width:100%;
-	  height: 100%;
+	 .bottom-tab{
+		height: 334rpx;
+		width:100%;
+		position: absolute;
+		bottom:0;
+		left:0;
+		display: flex;
+		flex-direction: column;
 	}
-	.container .bottom-tab{
-	  height: 334rpx;
-	  width:100%;
-	  position: absolute;
-	  bottom:0;
-	  left:0;
-	  display: flex;
-	  flex-direction: column;
+	 .tab{
+		height: 74rpx;
+		display: flex;
 	}
-	.container .tab{
-	  height: 74rpx;
-	  display: flex;
+	 .item{
+		flex: 1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 26rpx;
+		color:#ffffff;
+		background: rgba(0, 0, 0, .4);
 	}
-	.container .item{
-	  flex: 1;
-	  display: flex;
-	  align-items: center;
-	  justify-content: center;
-	  font-size: 26rpx;
-	  color:#ffffff;
-	  background: rgba(0, 0, 0, .4);
+	 .item.active{
+		color:#05c160;
+		background: rgba(0, 0, 0, .6);
 	}
-	.container .item.active{
-	  color:#05c160;
-	  background: rgba(0, 0, 0, .6);
-	}
-	.container .item image{
-	  width:36rpx;
-	  height:32rpx;
-	  margin-right: 20rpx;
+	 .item image{
+		width:36rpx;
+		height:32rpx;
+		margin-right: 20rpx;
 	}
 	
-	.container .bottom-tip{
-	  height: 60rpx;
-	  position: absolute;
-	  bottom: 0;
-	  left:0;
-	  padding:0 40rpx;
-	  box-sizing: border-box;
-	  width:100%;
-	  display: flex;
-	  align-items: center;
-	  justify-content: space-between;
-	  background: rgba(0, 0, 0, .6);
+	 .bottom-tip{
+		height: 60rpx;
+		padding:0 40rpx;
+		box-sizing: border-box;
+		width:100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		background: rgba(0, 0, 0, .6);
 	}
-	.container .tip-left{
-	  display: flex;
-	  align-items: center;
+	.content{
+		flex: 1;
+		display: flex;
+		padding:0 40rpx;
+		background: rgba(0, 0, 0, .6);
+		&-wechat{
+			flex: 1;
+			display: flex;
+			justify-content: center;
+			flex-direction: column;
+			button{
+				width:100%;
+				font-size:36rpx;
+				height:88rpx;
+				line-height:88rpx;
+			}
+		}
+		&-phone{
+			display: flex;
+			flex: 1;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			.form{
+				width:458rpx;
+				flex:1;
+			}
+			.btn{
+				width:170rpx;
+				height:62rpx;
+				border-radius:31rpx;
+				font-size:28rpx;
+				color:#ffffff;
+				background: #9ba0b0;
+			}
+		}
+		&-machine{}
 	}
-	.container .tip-left .cb{
-	  transform: scale(0.7);
-	  margin-right:20rpx;
+	
+	 .tip-left{
+		display: flex;
+		align-items: center;
 	}
-	.container .tip-left text{
-	  font-size:22rpx;
-	  color:#9e9e9f;
-	  text-decoration: underline;
+	 .tip-left .cb{
+		transform: scale(0.5);
 	}
+	 .tip-left text{
+		font-size:22rpx;
+		color:#9e9e9f;
+		text-decoration: underline;
+	}
+	.tip-right{
+		display: flex;
+		align-items: center;
+		.btn{
+			border:1rpx solid #ffffff;
+			font-size:22rpx;
+			color:#ffffff;
+			width:80rpx;
+			height:36rpx;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			border-radius:6rpx;
+			&:first-child{
+				margin-right:40rpx;
+			}
+		}
+	}
+}
 </style>
